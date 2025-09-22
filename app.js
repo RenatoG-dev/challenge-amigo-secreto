a3cb56baf1339a07909a30b816b1ec7dacca82f8
RenatoG-dev <renato.gama_f@icloud.com>
2025-09-22 23:08:52 +0100

    funcao atualizar lista de amigos

--- app.js
+++ app.js
@@ -23,3 +23,13 @@ function adicionarAmigo() {
   input.value = '';
   atualizarLista();
 }
+
+function atualizarLista() {
+  const lista = document.getElementById('listaAmigos');
+  lista.innerHTML = '';
+  for (let i = 0; i < amigos.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
+}
